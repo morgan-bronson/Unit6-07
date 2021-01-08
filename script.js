f3e22099cd0255e19d47203dad5a7b7d210bec6c
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-08 14:19:24 +0000

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ document.getElementById('mybtn').addEventListener('click', multiply)
 // Event Listener connects the button with the function "multiply()"
 
 function multiply () {
+  let question = 0*0
+  while (question) {
+    question = parseInt(prompt('multiply here!'))
+    // opens a prompt
+    question() {
+    }
+  }
+}
