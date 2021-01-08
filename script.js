b0ab91abb553b60769f8d921fee9470e21e63210
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-08 23:51:48 +0000

    errors

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ document.getElementById('mybtn').addEventListener('click', multiply)
 // Event Listener connects the button with the function "multiply()"
 
 function multiply () {
-  let multiplier = parseInt(document.getElementById("multiplier").value)
-  let multiplicand = parseInt(document.getElementById("multiplicand").value)
+  const multiplier = parseInt(document.getElementById('multiplier').value)
+  const multiplicand = parseInt(document.getElementById('multiplicand').value)
   let product = 0
   let i = 0
   if (multiplier > 0) {
     for (i = 1; i <= multiplier; i++) {
-      product=product+ multiplicand
+      product = product + multiplicand
     }
-  }
-  else {
+  } else {
     for (i = -1; i >= multiplier; i--) {
-      product=product - multiplicand
+      product = product - multiplicand
     }
   }
   alert('the answer is ' + product)
